b35638a87df8c1bf3d484732c8b088c519ad1289
allurinehasri <allurinehasri@gmail.com>
2025-11-03 16:09:46 +0530

    Update vpc1.tf

--- vpc1.tf
+++ vpc1.tf
@@ -5,7 +5,7 @@ module "vpc" {
   # version = "~> 2.78"
  
   # VPC Basic Details
-  name = "vpc-dev"
+  name = "vpcneha"
   cidr = "10.0.0.0/16"   
   azs                 = ["us-east-1a", "us-east-1b"]
   private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -45,6 +45,6 @@ module "vpc" {
   }
  
   vpc_tags = {
-    Name = "vpc-dev"
+    Name = "vpcneha"
   }
 }
